e5c7f8aee2607baa32fdd744bd558248c1ef14ad
jerry1100 <jwu1100@gmail.com>
2019-08-25 13:54:41 -0700

    use yargs.options() and add help messages

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const yargs = require('yargs');
 const path = require('path');
 
 const { sizes, input } = yargs
-  .alias('input', 'i')
-  .array('input')
-  .alias('sizes', 's')
-  .array('sizes')
+  .options({
+    input: {
+      alias: 'i',
+      array: true,
+      describe: 'File(s) to convert, e.g. icon.svg',
+      demandOption: true,
+    },
+    sizes: {
+      alias: 's',
+      array: true,
+      describe: 'Size(s) to generate, e.g. 32x32',
+      demandOption: true,
+    },
+  })
   .argv;
 
 console.log(input, sizes);
