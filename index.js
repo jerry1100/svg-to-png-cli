946dc1a30a9894668dc6f838bd826b08809c3de2
jerry1100 <jwu1100@gmail.com>
2019-08-25 15:01:45 -0700

    validate sizes

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const { inputs, sizes } = yargs
     inputs: {
       alias: 'i',
       array: true,
+      type: 'string',
       describe: 'File(s) to convert, e.g. icon.svg',
       demandOption: true,
     },
     sizes: {
       alias: 's',
       array: true,
+      type: 'string',
       describe: 'Size(s) to generate, e.g. 32x32',
       demandOption: true,
     },
   })
-  .check(({ inputs, sizes }) => validateInputs(inputs))
+  .check(({ inputs, sizes }) => validateInputs(inputs) && validateSizes(sizes))
   .argv;
 
 console.log(inputs, sizes);
@@ -32,4 +34,26 @@ function validateInputs(inputs) {
   }
 
   return true;
+}
+
+function validateSizes(sizes) {
+  const splitSizes = sizes.map(size => size.split('x'));
+
+  const sizeWithMissingDimensions = splitSizes.find(size => size.length < 2);
+
+  if (sizeWithMissingDimensions) {
+    throw new Error(`Error: Missing width or height for size "${sizeWithMissingDimensions}"`);
+  }
+
+  const sizeWithInvalidDimensions = splitSizes.find(dimensions => !isValidDimensions(dimensions));
+
+  if (sizeWithInvalidDimensions) {
+    throw new Error(`Error: Invalid width or height for size "${sizeWithInvalidDimensions.join('x')}"`);
+  }
+
+  return true;
+}
+
+function isValidDimensions([width, height]) {
+  return !isNaN(width) && !isNaN(height) && Number(width) > 0 && Number(height) > 0;
 }
