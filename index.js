bf006ff4cc9b6b597cd76cacf8955d7c8db51305
jerry1100 <jwu1100@gmail.com>
2019-09-06 19:39:01 -0700

    fix pngs generated in script dir, rename function

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const { inputs, sizes } = require('./get-args');
 
 inputs.forEach(async file => {
   const browser = await puppeteer.launch();
-  await Promise.all(sizes.map(dimensions => takeScreenshot(browser, file, dimensions)));
+  await Promise.all(sizes.map(dimensions => generatePNG(browser, file, dimensions)));
   await browser.close();
 });
 
-async function takeScreenshot(browser, file, { width, height }) {
-  const outPath = path.join(__dirname, `${path.basename(file, '.svg')}_${width}x${height}.png`);
+async function generatePNG(browser, file, { width, height }) {
+  const pngName = `${path.basename(file, '.svg')}${width}x${height}.png`;
   const page = await browser.newPage();
   await page.goto(`file://${file}`);
   await page.setViewport({ width, height });
-  await page.screenshot({ path: outPath, omitBackground: true });
+  await page.screenshot({ path: pngName, omitBackground: true });
 }
